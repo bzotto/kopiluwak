4fa1ccd11d98d7276a348b0b21169a6982ce00a9
bzotto <bzotto@gmail.com>
2020-11-30 21:59:55 -0500

    Fix JS <-> UTF16 non-BMP encode/decode Tfw you do the hard thing because you assume you have to, and it's wrong, before realizing that the easy thing would have worked the whole time.
    Broken version was fine for BMP code points but JS already knows about surrogate pairs
    when you ask it for character codes in a string! Now emojis work, I guess.

--- utf.js
+++ utf.js
@@ -4,26 +4,17 @@
 // Kopiluwak. Copyright (c) 2020 Ben Zotto
 //
 
+//
+// Conversion between UTF16 and JS strings is trivial; the JS string's length and char code
+// concepts are already relative to the code points, including the surrogate pairs where
+// required. We just need to convert between the byte pairs and the char codes.
+//
+
 function KLStringFromUTF16Array(bytes) {
 	let str = "";
-	for (let i = 0; i < bytes.length;) {
+	for (let i = 0; i < bytes.length; i+=2) {
 		let word = ((bytes[i] << 8) | bytes[i+1]) >>>0;
-		if (word >= 0xDC00 && word < 0xE000) { // a low surrogate is only invalid value for this pair.
-			return null;
-		}
-		if (word < 0xDC00 || word >= 0xE000) {
-			str += String.fromCharCode(word);
-			i += 2;
-		} else {
-			let w1 = word;
-			let w2 = ((bytes[i+2] << 8) | bytes[i+3]) >>>0;
-			let highbits = w1 & 0x3FF;
-			let lowbits = w2 & 0x3FF;
-			let chprime = ((w1 << 10) | w2) >>>0;
-			let ch = chprime + 0x10000;
-			str += String.fromCharCode(ch);
-			i += 4;
-		}
+		str += String.fromCharCode(word);
 	}
 	return str;
 }
@@ -32,20 +23,8 @@ function KLUTF16ArrayFromString(str) {
 	let bytes = [];
 	for (let i = 0; i < str.length; i++) {
 		let ch = str.charCodeAt(i);
-		if (ch < 0xD800 || (ch >= 0xE000 && ch < 0x10000)) {
-			bytes.push((ch >> 8) & 0xFF);
-			bytes.push(ch & 0xFF);
-		} else {
-			let chprime = ch - 0x10000;
-			let highbits = (chprime >> 10) & 0x3FF;
-			let lowbits = chprime & 0x3FF;
-			let w1 = 0xD800 + highbits;
-			let w2 = 0xDC00 + lowbits;
-			bytes.push((w1 >> 8) & 0xFF);
-			bytes.push(w1 & 0xFF);
-			bytes.push((w2 >> 8) & 0xFF);
-			bytes.push(w2 & 0xFF);
-		}
+		bytes.push((ch >> 8) & 0xFF);
+		bytes.push(ch & 0xFF);
 	}
 	return bytes;
 }
